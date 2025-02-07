dc80a3644d3cc3f486dc27e9446391ef90918f77
Fabian Andiel <fabian.andiel@gmail.com>
2025-02-07 12:16:06 +0100

    Adds social media functionality for handy image

--- components/links/HandyImage.tsx
+++ components/links/HandyImage.tsx
@@ -1,30 +1,30 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import "./handyImage.scss";
 import Image from "next/image";
-import Github from "/social-media-icons/icon-github.svg";
+import Github from "public/social-media-icons/icon-github.svg";
 
-const HandyImage = () => {
-  const GitHubIcon = () => (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="16"
-      height="16"
-      fill="none"
-      viewBox="0 0 16 16"
-    >
-      <g clip-path="url(#a)">
-        <path
-          fill="#737373"
-          d="M9.982 2.288a8.756 8.756 0 0 0-3.963 0c-.754-.462-1.329-.674-1.747-.764a2.315 2.315 0 0 0-.544-.056 1.342 1.342 0 0 0-.247.03l-.01.002-.005.002h-.003l.146.513-.146-.512a.533.533 0 0 0-.342.294 3.328 3.328 0 0 0-.17 2.241 3.578 3.578 0 0 0-.817 2.287c0 1.657.488 2.77 1.321 3.486.584.501 1.292.768 2.002.92a2.496 2.496 0 0 0-.123 1.022v.638c-.434.09-.735.062-.95-.008-.267-.089-.473-.267-.67-.523a5.118 5.118 0 0 1-.289-.429l-.06-.099a9.772 9.772 0 0 0-.24-.378c-.202-.3-.503-.675-.99-.803l-.515-.135-.271 1.032.516.136c.085.021.196.101.379.369.07.106.137.213.202.322l.073.117c.1.162.215.342.349.517.27.352.637.707 1.184.887.373.124.797.154 1.282.079v1.992a.533.533 0 0 0 .533.533h4.267a.533.533 0 0 0 .533-.534v-3.8c0-.336-.015-.644-.11-.931.707-.15 1.41-.416 1.99-.918.833-.72 1.32-1.845 1.32-3.511v-.001a3.578 3.578 0 0 0-.82-2.267 3.328 3.328 0 0 0-.169-2.24.533.533 0 0 0-.34-.295l-.146.512c.146-.512.145-.512.144-.512l-.002-.001-.005-.002-.01-.003a1.344 1.344 0 0 0-.248-.03 2.318 2.318 0 0 0-.544.057c-.417.09-.992.302-1.745.764Z"
-        />
-      </g>
-      <defs>
-        <clipPath id="a">
-          <path fill="#fff" d="M0 0h16v16H0z" />
-        </clipPath>
-      </defs>
-    </svg>
-  );
+const HandyImage: React.FC = () => {
+  const RowWithSocialMedia = (): ReactNode => {
+    return (
+      <>
+        <rect width="237" height="44" x="35" y="278" fill="#EEE" rx="8" />
+        <foreignObject x="35" y="278" width="237" height="44">
+          <div className="social-media-content">
+            <div>
+              <Github />
+              <span>GitHub</span>
+            </div>
+            <Image
+              src={"/icon-arrow-right.svg"}
+              width="16"
+              height="16"
+              alt="arrow-right"
+            />
+          </div>
+        </foreignObject>
+      </>
+    );
+  };
 
   return (
     <div>
@@ -46,31 +46,8 @@ const HandyImage = () => {
         />
         <circle cx="153.5" cy="112" r="48" fill="#EEE" />
         <rect width="160" height="16" x="73.5" y="185" fill="#EEE" rx="8" />
-
         <rect width="72" height="8" x="117.5" y="214" fill="#EEE" rx="4" />
         <rect width="237" height="44" x="35" y="278" fill="#EEE" rx="8" />
-        <foreignObject x="35" y="278" width="237" height="44">
-          <div
-            className="social-media-content"
-            xmlns="http://www.w3.org/1999/xhtml"
-          >
-            <div>
-              <Image
-                src={"/social-media-icons/icon-github.svg"}
-                width="16"
-                height="16"
-                alt="arrow-right"
-              />
-              <span style={{ marginLeft: "8px" }}>GitHub</span>
-            </div>
-            <Image
-              src={"/icon-arrow-right.svg"}
-              width="16"
-              height="16"
-              alt="arrow-right"
-            />
-          </div>
-        </foreignObject>
         <rect width="237" height="44" x="35" y="342" fill="#EEE" rx="8" />
         <rect width="237" height="44" x="35" y="406" fill="#EEE" rx="8" />
         <rect width="237" height="44" x="35" y="470" fill="#EEE" rx="8" />
--- next.config.js
+++ next.config.js
@@ -5,6 +5,13 @@ const nextConfig = {
     includePaths: [path.join(__dirname, "app")],
     prependData: `@import "variables";`,
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
